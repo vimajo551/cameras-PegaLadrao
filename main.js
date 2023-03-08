aec68670552efb3d5203e608484146f1970e4034
vimajo551 <104533881+vimajo551@users.noreply.github.com>
2023-03-08 20:48:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+status = ""
+objects = []
+
+
 function preload(){
 
 }
@@ -8,5 +12,16 @@ function setup(){
 
 function draw(){
     
+    if(status != ""){
+        for(var i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "status: objeto detectado"   
+            fill("#000000")
+            percente = floor(objects[i].confidence*100)
+            text(objects[i].label + " " + percente + "%", objects[i].x, objects[i].y)
+            noFill()
+            stroke("#FF0000")
+            rect(objects[i].x ,objects[i].y ,objects[i].width ,objects[i].height)
+        }
+    }
 }
 
